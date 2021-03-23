2176ab52e01856977a816257cb1f0e635794e5c0
chocoCO3 <71392348+SKT-T1-BUSH@users.noreply.github.com>
2021-03-23 11:48:28 +0900

    Create Odd_Even.cpp

---
+++ Level1/Odd_Even.cpp
@@ -0,0 +1,17 @@
+  #include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(int num) {
+    if(num%2==0)
+    {
+        string answer = "Even";
+        return answer;
+    }
+    else
+    {
+        string answer = "Odd";
+        return answer;
+    }
+}
